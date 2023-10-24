a9ef119021717873d7187e7dc89c0048435eb8ad
lee jihyeon <55480011+JiHyeonL@users.noreply.github.com>
2023-10-25 01:16:01 +0900

    [Gold II] Title: 보석 도둑, Time: 1528 ms, Memory: 129844 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,48 +1,58 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.*;
- 
+
+// 1202번
 public class Main {
-    
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
- 
-        int n = scan.nextInt();
-        int k = scan.nextInt();
-        
-        ArrayList<Gem> list = new ArrayList<>();
-        for(int i = 0; i < n; i++) {
-            int m = scan.nextInt();
-            int v = scan.nextInt();
-            list.add(new Gem(m, v));
+    public static int N;    // 보석 개수
+    public static int K;    // 가방 개수
+    public static ArrayList<Info> jewel = new ArrayList<>(); // 보석의 무게, 가격이 들어있는 리스트 (o1,o2) -> o2.price - o1.price
+    public static ArrayList<Integer> bag = new ArrayList<>();
+
+    static class Info {
+        int weight;
+        int price;
+
+        Info(int weight, int price) {
+            this.weight = weight;
+            this.price = price;
         }
-        Collections.sort(list, (o1, o2) -> o1.m - o2.m); 
-        
-        int[] weight = new int[k];
-        for(int i = 0; i < k; i++) {
-            weight[i] = scan.nextInt();
+    }
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            jewel.add(new Info(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())));
         }
-        Arrays.sort(weight);
-        
-        long total = 0;
-        int listIdx = 0;
-        PriorityQueue<Gem> pq = new PriorityQueue<>((o1, o2) -> o2.v - o1.v); //가격순 정렬
-        for(int i = 0; i < k; i++) {
-            while(listIdx < n && list.get(listIdx).m <= weight[i]) {
-                Gem current = list.get(listIdx++);
-                pq.add(new Gem(current.m, current.v));
+        for (int j = 0; j < K; j++) {
+            bag.add(Integer.parseInt(br.readLine()));
+        }
+        Collections.sort(jewel,(o1,o2) -> o1.weight-o2.weight);
+        Collections.sort(bag);
+
+        PriorityQueue<Info> priorityQueue = new PriorityQueue<>((o1,o2) -> o2.price - o1.price);
+        long maxPrice = 0;
+        int index = 0;
+        for (int i = 0; i < K; i++) {
+            while (index < N && jewel.get(index).weight <= bag.get(i)) {
+                priorityQueue.offer(new Info(jewel.get(index).weight,jewel.get(index).price));
+                index++;
             }
-            if(!pq.isEmpty()) total += pq.poll().v;
+            if (!priorityQueue.isEmpty()) {
+                maxPrice += priorityQueue.poll().price;
+            }
         }
-        System.out.println(total);
+        System.out.println(maxPrice);
     }
-    
-    public static class Gem {
-        int m;
-        int v;
-        
-        public Gem(int m, int v) {
-            this.m = m;
-            this.v = v;
-        }
-    }
+
 }
- 
+
