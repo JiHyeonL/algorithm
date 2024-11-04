4dbc22df16da651069d1514385662d2e38a0163d
lemone <55480011+JiHyeonL@users.noreply.github.com>
2024-11-04 18:07:42 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,30 +1,52 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
 
-// 2839번
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+
+    public static int n;
+    public static int[] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        // 5로 나누어 떨어질 때까지 3을 빼준다.
-        int count = 0;
-        while (n > 2) {
-            // 5로 나누어 떨어지는 경우
-            if (n % 5 == 0) {
-                count += n / 5;
-                break;
-            }
-            // 3으로 나누어 떨어지지 않는 경우 -> -1 반환
-            if (n < 5 && n % 3 != 0) {
-                count = -1;
-                break;
-            }
-            n -= 3;
-            count++;
-        }
-        System.out.println(count);
+        n = Integer.parseInt(br.readLine());
+        dp = new int[n+3];
+        System.out.println(answer());
     }
 
+    private static int answer() {
+        dp[0] = -1;
+        dp[1] = -1;
+        dp[2] = -1;
+        dp[3] = 1;
+        dp[4] = -1;
+        dp[5] = 1;
+
+        if (n <= 5) {
+            return dp[n];
+        }
+        // 식: dp[i] = min(dp[i-5] or dp[i-3]) + 1
+        for (int i = 6; i <= n; i++) {
+            // 규칙: 둘 중 하나라도 되면 그 값 + 1
+            if (dp[i - 5] != -1 || dp[i - 3] != -1) {
+                if (dp[i - 5] != -1 && dp[i - 3] != -1) {
+                    dp[i] = Math.min(dp[i - 5], dp[i - 3]) + 1;
+                    //System.out.println("둘 중 작은값 i: "+i + ", "+ dp[i]);
+                }
+                else if (dp[i - 5] != -1) {
+                    dp[i] = dp[i - 5] + 1;
+                    //System.out.println("5이전거 i: "+i + ", "+ dp[i]);
+                }
+                else {
+                    dp[i] = dp[i - 3] + 1;
+                    //System.out.println("3이전거 i: "+i + ", "+ dp[i]);
+                }
+                continue;
+            }
+            dp[i] = -1;
+            //System.out.println("i: "+i + ", "+dp[i]);
+        }
+        return dp[n];
+    }
 }
