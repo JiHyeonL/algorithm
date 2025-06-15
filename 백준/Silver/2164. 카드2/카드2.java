f0d2e2a044c756964b1ebd3b951ecc4ea1c731be
JiHyeon Lee(이지현) <55480011+JiHyeonL@users.noreply.github.com>
2025-06-16 00:07:36 +0900

    [Silver IV] Title: 카드2, Time: 168 ms, Memory: 53396 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+public class Main {
+
+    private static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size() > 1) {
+            queue.poll();
+            if (queue.size() == 1) {
+                break;
+            }
+            int bottom = queue.poll();
+            queue.add(bottom);
+        }
+
+        System.out.println(queue.poll());
+    }
+}
